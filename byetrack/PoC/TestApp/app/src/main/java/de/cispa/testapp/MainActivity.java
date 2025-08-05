f1895b16afac8cf5c1a4ee679cf4443e8e215848
timchr42 <timjannis03@gmail.com>
2025-08-06 00:04:36 +0200

    fixed displaying of tokens after wiping data (s.t new tokens from fresh installs take over by deleting old ones)

--- byetrack/PoC/TestApp/app/src/main/java/de/cispa/testapp/MainActivity.java
+++ byetrack/PoC/TestApp/app/src/main/java/de/cispa/testapp/MainActivity.java
@@ -4,12 +4,9 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.browser.customtabs.CustomTabsIntent;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.net.Uri;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,7 +14,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private static final String LOGTAG = "TestApp";
     private TextView statusText;
-    private TextView capStorage;
+    public TextView capStorage;
     private final Context mContext = MainActivity.this;
     private TokenManager mTokenManager;
 
@@ -49,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String url = "http://10.0.2.2/"; // examplecorp.de -> 10.0.2.2 on emulator
-                launchUrlMod(mContext, Uri.parse(url));
+                mTokenManager.launchUrlMod(mContext, Uri.parse(url));
                 //Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
                 //i.setPackage("org.mozilla.geckoview_example");
                 //startActivity(i);
@@ -60,48 +57,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String url = "https://royaleapi.com";
-                launchUrlMod(mContext, Uri.parse(url));
+                mTokenManager.launchUrlMod(mContext, Uri.parse(url));
             }
         });
-    }
-
-
-    /**
-     * Launches a regular CustomTab if no Capabilities exist for domain, else attaches them to to the intent
-     * @param uri to launch CustomTab for
-     */
-    private void launchUrlMod(Context context, Uri uri) {
-        // Build CustomTabsIntent
-        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-        CustomTabsIntent customTabsIntent = builder.build();
-        // customTabsIntent.intent.setPackage("org.mozilla.geckoview_example"); -> Make Firefox (GeckoView Example) default browser
-
-        attachCapsToIntent(customTabsIntent, uri); // getDomainName broken for name like 10.0.2.2
-        customTabsIntent.launchUrl(context, uri);
-    }
-
-    /**
-     * Attaches Capabilities to Intent if available
-     * @param customTabsIntent Intent to include Capabilities to
-     * @param uri to launch CustomTab for
-     */
-    private void attachCapsToIntent(CustomTabsIntent customTabsIntent, Uri uri) {
-        SharedPreferences prefs = getSharedPreferences("cap_storage", MODE_PRIVATE);
-        String domainName = getDomainName(uri);
-        String caps = prefs.getString(domainName, null);
-        if (caps != null && !caps.isEmpty()) {
-            customTabsIntent.intent.putExtra("browser_capability", caps);
-            Log.v(LOGTAG, "Capability found for " + domainName);
-        } else {
-            Log.v(LOGTAG, "No capability found for " + domainName);
-        }
-    }
-    @SuppressLint("SetTextI18n")
-    private String getDomainName(Uri uri) {
-        String host = uri.getHost();
-        assert host != null;
-        statusText.setText("Domain: " + host);
-        return host;
     }
 
 }
--- byetrack/PoC/TestApp/app/src/main/java/de/cispa/testapp/TokenManager.java
+++ byetrack/PoC/TestApp/app/src/main/java/de/cispa/testapp/TokenManager.java
@@ -2,12 +2,16 @@ package de.cispa.testapp;
 
 import static android.content.Context.MODE_PRIVATE;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.util.Log;
+
+import androidx.browser.customtabs.CustomTabsIntent;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,7 +23,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class TokenManager {
-    private static final String LOGTAG = "TokenManager";
+    private static final String LOGTAG = "TestApp-TokenManager";
     private static final String ACTION = "org.mozilla.geckoview.CAPABILITY_TOKENS";
     private final Context mContext;
     private boolean mReceiverRegistered = false;
@@ -59,6 +63,9 @@ public class TokenManager {
     };
 
     private void storeTokens(String tokenJson) {
+        SharedPreferences prefs = mContext.getSharedPreferences("cap_storage", MODE_PRIVATE);
+        prefs.edit().clear().apply(); // Wipe data => no "old" tokens remain after update
+
         try {
             JSONObject tokens = new JSONObject(tokenJson);
 
@@ -67,7 +74,7 @@ public class TokenManager {
             while (domains.hasNext()) {
                 String domain = domains.next();
                 JSONArray domainTokens = tokens.getJSONArray(domain);
-                storeTokensForDomain(domain, domainTokens);
+                storeTokensForDomain(domain, domainTokens, prefs);
             }
             Log.d(LOGTAG, "Tokens successfully stored for all domains");
         } catch (Exception e) {
@@ -81,20 +88,57 @@ public class TokenManager {
      * @param tokens The token to be stored for domain
      * @param domain The domain tokens are associated to
      */
-    private void storeTokensForDomain(String domain, JSONArray tokens) throws JSONException {
-        SharedPreferences prefs = mContext.getSharedPreferences("cap_storage", MODE_PRIVATE);
-        prefs.edit().clear().apply(); // Wipe data => no "old" tokens remain after update
+    private void storeTokensForDomain(String domain, JSONArray tokens, SharedPreferences prefs) throws JSONException {
         // Get existing tokens for this domain (if any)
-       //  Set<String> tokenSet = new HashSet<>(prefs.getStringSet(domain, new HashSet<>()));
-        Set<String> tokenSet = new HashSet<>();
+        Set<String> tokenSet = new HashSet<>(prefs.getStringSet(domain, new HashSet<>()));
+        // Set<String> tokenSet = new HashSet<>();
 
         // Add all new tokens from the JSONArray
         for (int i = 0; i < tokens.length(); i++) {
-            tokenSet.add(tokens.getString(i)); // Set ensures no duplicates
+            tokenSet.add(tokens.getString(i));
         }
 
         // Store the updated set back into SharedPreferences
         prefs.edit().putStringSet(domain, tokenSet).apply();
+        Log.d(LOGTAG, "Stored " + tokenSet + " for " + domain);
+    }
+
+
+    /**
+     * Launches a regular CustomTab if no Capabilities exist for domain, else attaches them to to the intent
+     * @param uri to launch CustomTab for
+     */
+    public void launchUrlMod(Context context, Uri uri) {
+        // Build CustomTabsIntent
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        CustomTabsIntent customTabsIntent = builder.build();
+        // customTabsIntent.intent.setPackage("org.mozilla.geckoview_example"); -> Make Firefox (GeckoView Example) default browser
+
+        attachCapsToIntent(customTabsIntent, uri); // getDomainName broken for name like 10.0.2.2
+        customTabsIntent.launchUrl(context, uri);
+    }
+
+    /**
+     * Attaches Capabilities to Intent if available
+     * @param customTabsIntent Intent to include Capabilities to
+     * @param uri to launch CustomTab for
+     */
+    private void attachCapsToIntent(CustomTabsIntent customTabsIntent, Uri uri) {
+        SharedPreferences prefs = mContext.getSharedPreferences("cap_storage", MODE_PRIVATE);
+        String domainName = getDomainName(uri);
+        String caps = prefs.getString(domainName, null);
+        if (caps != null && !caps.isEmpty()) {
+            customTabsIntent.intent.putExtra("browser_capability", caps);
+            Log.v(LOGTAG, "Capability found for " + domainName);
+        } else {
+            Log.v(LOGTAG, "No capability found for " + domainName);
+        }
+    }
+    @SuppressLint("SetTextI18n")
+    private String getDomainName(Uri uri) {
+        String host = uri.getHost();
+        assert host != null;
+        return host;
     }
 
     public String displayCapabilities() {
